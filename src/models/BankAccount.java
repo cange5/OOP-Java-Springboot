2bf3a5bad432238e40bdfbfad9e3aa97f8f21216
Carlo <cangelucci@itsmeccanicabruzzo.eu>
2023-06-20 12:07:38 +0200

    Nuovo esercizio: BankAccount

--- src/models/BankAccount.java
+++ src/models/BankAccount.java
@@ -3,16 +3,28 @@ package models;
 public class BankAccount {
     static String bankName;
     String accountNumber;
+    double balance;
 
-    static void setBankName(String name){
-        BankAccount.bankName=name;
+    public static void setBankName(String name){
+       bankName=name;
     }
 
     public void deposit(double amount){
-
+        this.balance=amount;
     }
 
-    public void withDraw(double amount){
+    public double withDraw(double amount){
+        if(amount < this.balance){
+            this.balance=this.balance-amount;
+            System.out.println("Operazione avvenuta con successo!");
+            return this.balance;
+        }else{
+            System.out.println("Non hai abbastanza soldi sul conto!");
+            return this.balance;
+        }
+    }
+
+    public void getAccountInfo(){
 
     }
 }
